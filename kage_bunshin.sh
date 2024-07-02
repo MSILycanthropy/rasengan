60153b3b06b9f6895a85640631c0af3b8dba713b
Ethan Kircher <ethanmichaelk@gmail.com>
2024-07-01 21:16:42 -0500

    platforms

---
+++ kage_bunshin.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [ -n "$1" ]; then
+  valid_platforms=("windows" "macos" "ubuntu")
+  valid=false
+
+  for item in "${valid_platforms[@]}"; do
+    if [[ "$item" == "$1" ]]; then
+      valid=true
+      break
+    fi
+  done
+
+  if $valid; then
+    PLATFORM=$1
+  else
+    echo "$1 is not a valid platform (${valid_platforms[@]})"
+    exit 1
+  fi
+else
+  PLATFORM="windows"
+fi
+
+OUT_DIR=executables/amogus
+URL=$(
+  curl -s https://api.github.com/repos/xinntao/Real-ESRGAN/releases \
+    | grep "browser_download_url" \
+    | grep "$PLATFORM.zip" \
+    | head -n 1 \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+)
+FILE=$(basename $URL)
+
+echo $URL | wget -O $FILE -qi - | wait
+
+if [ -f "$FILE" ]; then
+  mdkir $OUT_DIR
+  echo "Unzipping $FILE..."
+  unzip $FILE -d $OUT_DIR >/dev/null 2>&1
+  rm -rf $FILE
+  echo "All done."
+else
+  echo "Something went wrong.. please try again."
+fi
--- scripts/upscale.sh
+++ scripts/upscale.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# executables/waifu2x/waifu2x-ncnn-vulkan.exe -i tmp_frames -o out_frames -s 2 -f jpg -g 0 -j 4:4:4
-executables/realesrgan/realesrgan-ncnn-vulkan.exe -i tmp_frames -o out_frames -s "$1" -f jpg -g 0 -j 4:4:4 > /dev/null 2>&1
-# executables/anime4k/Anime4KCPP_CLI.exe -i tmp_frames -o out_frames -z 4 -w -q -H
+if [ -f "executables/realesrgan/realesrgan-ncnn-vulkan.exe" ]; then
+  # executables/anime4k/Anime4KCPP_CLI.exe -i tmp_frames -o out_frames -z 4 -w -q -H
+  EXECUTABLE=executables/realesrgan/realesrgan-ncnn-vulkan.exe
+elif [ -f "executables/realesrgan/realesrgan-ncnn-vulkan" ]; then
+  EXECUTABLE=executables/realesrgan/realesrgan-ncnn-vulkan
+fi
+
+$EXECUTABLE -i tmp_frames -o out_frames -s "$1" -f jpg -g 0 -j 4:4:4 > /dev/null 2>&1
